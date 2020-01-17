171213f0690e1e468634520f4ea6f0ec66a0eb8f
wolf <panhaoyu.china@outlook.com>
2020-01-17 12:19:19 +0800

    完成了数据库的构建

---
+++ database.py
@@ -0,0 +1,47 @@
+import peewee
+
+database = peewee.SqliteDatabase('db.sqlite3')
+
+
+class Province(peewee.Model):
+    class Meta:
+        database = database
+
+    name = peewee.CharField(unique=True)
+    url = peewee.CharField()
+
+
+class Station(peewee.Model):
+    class Meta:
+        database = database
+
+    province = peewee.ForeignKeyField(Province)
+    name = peewee.CharField(unique=True)
+    url = peewee.CharField()
+
+
+class Train(peewee.Model):
+    class Meta:
+        database = database
+
+    name = peewee.CharField(unique=True)
+    url = peewee.CharField()
+
+
+class TimeTable(peewee.Model):
+    class Meta:
+        database = database
+
+    train = peewee.ForeignKeyField(Train)
+    station = peewee.ForeignKeyField(Station)
+    arrive = peewee.TimeField()
+    leave = peewee.TimeField()
+
+
+if __name__ == '__main__':
+    database.create_tables([
+        Province,
+        Station,
+        Train,
+        TimeTable,
+    ])
